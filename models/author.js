e74e77a7269695e4954303e597b59b00ab20c488
Ryutaro Kawakami <ne200152g@gmail.com>
2018-01-11 14:50:35 +0900

    add genre list page

--- models/author.js
+++ models/author.js
@@ -19,4 +19,12 @@ AuthorSchema.virtual('url').get(function () {
 	return `/catalog/author/${this._id}`;
 });
 
+AuthorSchema.virtual('due_back_date_of_birth').get(function () {
+	return this.date_of_birth ? moment(this.date_of_birth).format('MMMM Do, YYYY') : '';
+});
+
+AuthorSchema.virtual('due_back_date_of_death').get(function () {
+	return this.date_of_death ? moment(this.date_of_death).format('MMMM Do, YYYY') : '';
+});
+
 module.exports = mongoose.model('Author', AuthorSchema);
