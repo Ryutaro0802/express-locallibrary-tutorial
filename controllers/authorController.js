204e4c12fa030827379a56f54d208030054c6e21
Ryutaro Kawakami <ne200152g@gmail.com>
2018-01-24 09:54:30 +0900

    working-with-forms

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,6 +1,9 @@
-const async = require('async');
-const Author = require('../models/author');
-const Book = require('../models/book');
+const async = require('async')
+const Author = require('../models/author')
+const Book = require('../models/book')
+
+const { body, validationResult } = require('express-validator/check')
+const { sanitizeBody } = require('express-validator/filter')
 
 // Display list of all Authors
 
@@ -8,11 +11,11 @@ exports.author_list = (req, res, next) => {
 	Author.find()
 		.sort([['family_name', 'ascending']])
 		.exec((err, list_authors) => {
-			if (err) { return next(err); }
+			if (err) { return next(err) }
 
-			res.render('author_list', { title: 'Author List', author_list: list_authors });
-		});
-};
+			res.render('author_list', { title: 'Author List', author_list: list_authors })
+		})
+}
 
 // Display detail page for a specific Author
 
@@ -20,58 +23,96 @@ exports.author_detail = (req, res, next) => {
 	async.parallel({
 		author: (callback) => {
 			Author.findById(req.params.id)
-				.exec(callback);
+				.exec(callback)
 		},
 
 		authors_books: (callback) => {
 			Book.find({ 'author': req.params.id }, 'title summary')
-				.exec(callback);
+				.exec(callback)
 		}
 	}, (err, results) => {
-		if (err) { return next(err); }
+		if (err) { return next(err) }
 
 		if (results.author == null) {
-			const err = new Error('著者は見つかりませんでした');
-			err.status = 404;
-			return next(err);
+			const err = new Error('著者は見つかりませんでした')
+			err.status = 404
+			return next(err)
 		}
 
-		res.render('author_detail', { title: 'Author Detail', author: results.author, author_books: results.authors_books });
-	});
-};
+		res.render('author_detail', { title: 'Author Detail', author: results.author, author_books: results.authors_books })
+	})
+}
 
 // Display Author create form on GET
 
 exports.author_create_get = (req, res) => {
-	res.send('NOT IMPLEMENTED: Author create GET');
-};
+	res.render('author_form', { title: 'Create Author' })
+}
 
 // Handle Author create on POST
 
-exports.author_create_post = (req, res) => {
-	res.send('NOT IMPLEMENTED: Author create POST');
-};
+exports.author_create_post = [
+	// Validation
+
+	body('first_name')
+		.isLength({ min: 1 }).trim().withMessage('First name must be specified.')
+		.isAlphanumeric().withMessage('Frist name has non-alphanumeric characters'),
+	body('family_name')
+		.isLength({ min: 1 }).trim().withMessage('Family name must be specfied.')
+		.isAlphanumeric().withMessage('Family name has non-alphanumeric characters'),
+	body('date_of_birth', 'Invalid date of birth').optional({ checkFalsy: true }).isISO8601(),
+	body('date_of_death', 'Invalid date of death').optional({ checkFalsy: true }).isISO8601(),
+
+	// Sanitize fields.
+
+	sanitizeBody('first_name').trim().escape(),
+	sanitizeBody('family_name').trim().escape(),
+	sanitizeBody('date_of_birth').toDate(),
+	sanitizeBody('date_of_death').toDate(),
+	
+	(req, res, next) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {	// エラーがあったとき
+			res.render('author_form', {title: 'Create Author', author: req.body, errors: errors.array()})
+		} else {	// エラーがなかったとき
+			const author = new Author({
+				first_name: req.body.first_name,
+				family_name: req.body.family_name,
+				date_of_birth: req.body.date_of_birth,
+				date_of_death: req.body.date_of_death
+			})
+
+			author.save((err) => {
+				if (err) { return next(err) }
+				
+				res.redirect(author.url);
+			})
+		}
+	}
+]
+
+
 
 // Display Author delete form on GET
 
 exports.author_delete_get = (req, res) => {
-	res.send('NOT IMPLEMENTED: Author delete GET');
-};
+	res.send('NOT IMPLEMENTED: Author delete GET')
+}
 
 // Handle Author delete on POST
 
 exports.author_delete_post = (req, res) => {
-	res.send('NOT IMPLEMENTED: Author delete POST');
-};
+	res.send('NOT IMPLEMENTED: Author delete POST')
+}
 
 // Display Author update form on GET
 
 exports.author_update_get = (req, res) => {
-	res.send('NOT IMPLEMENTED: Author update GET');
-};
+	res.send('NOT IMPLEMENTED: Author update GET')
+}
 
 // Handle Author update on POST
 
 exports.author_update_post = (req, res) => {
-	res.send('NOT IMPLEMENTED: Author update POST');
-};
+	res.send('NOT IMPLEMENTED: Author update POST')
+}
--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,4 +1,8 @@
-const BookInstance = require('../models/bookinstance');
+const BookInstance = require('../models/bookinstance')
+const Book = require('../models/book')
+
+const { body, validationResult } = require('express-validator/check')
+const { sanitizeBody } = require('express-validator/filter')
 
 // Display list of all BookInstances
 
@@ -6,11 +10,11 @@ exports.bookinstance_list = (req, res, next) => {
 	BookInstance.find()
 		.populate('book')
 		.exec((err, list_bookinstances) => {
-			if (err) { return next(err); }
-			
-			res.render('bookinstance_list', { title: '本の在庫リスト', bookinstance_list: list_bookinstances });
-		});
-};
+			if (err) { return next(err) }
+
+			res.render('bookinstance_list', { title: '本の在庫リスト', bookinstance_list: list_bookinstances })
+		})
+}
 
 // Display detail page for a specific BookInstance
 
@@ -18,52 +22,100 @@ exports.bookinstance_detail = (req, res, next) => {
 	BookInstance.findById(req.params.id)
 		.populate('book')
 		.exec((err, bookinstance) => {
-			if (err) { return next(err); }
+			if (err) { return next(err) }
 
 			if (BookInstance == null) {
-				const err = new Error('本の在庫は見つかりませんでした');
+				const err = new Error('本の在庫は見つかりませんでした')
 
-				err.status = 404;
-				
-				return next(err);
+				err.status = 404
+
+				return next(err)
 			}
 
-			res.render('bookinstance_detail', { title: 'Book:', bookinstance: bookinstance})
-		});
-};
+			res.render('bookinstance_detail', { title: 'Book:', bookinstance: bookinstance })
+		})
+}
 
 // Display BookInstance create form on GET
 
 exports.bookinstance_create_get = (req, res) => {
-	res.send('NOT IMPLEMENTED: BookInstance create GET');
-};
+	Book.find({}, 'title')
+		.exec((err, books) => {
+			if (err) { return next(err) }
+			res.render('bookinstance_form', { title: 'Create BookInstance', book_list: books })
+		})
+}
 
 // Handle BookInstance create on POST
 
-exports.bookinstance_create_post = (req, res) => {
-	res.send('NOT IMPLEMENTED: BookInstance create POST');
-};
+exports.bookinstance_create_post = [
+	// Validate
+
+	body('book', 'Book must be specified').isLength({ min: 1 }).trim(),
+	body('imprint', 'Imprint must be specified').isLength({ min: 1 }).trim(),
+	body('due_back', 'Invalid date').optional({ checkFalsy: true }).isISO8601(),
+
+	// Sanitize
+
+	sanitizeBody('book').trim().escape(),
+	sanitizeBody('imprint').trim().escape(),
+	sanitizeBody('status').trim().escape(),
+	sanitizeBody('due_back').trim().toDate(),
+
+	(req, res, next) => {
+		const errors = validationResult(req)
+
+		const bookinstance = new BookInstance({
+			book: req.body.book,
+			imprint: req.body.imprint,
+			status: req.body.status,
+			due_back: req.body.due_back
+		})
+
+		if (!errors.isEmpty()) {
+			Book.find({}, 'title')
+				.exec((err, books) => {
+					if (err) { return next(err) }
+					res.render('bookinstance_form', {
+						title: 'Create BookInstance',
+						book_list: books,
+						selected_book: bookinstance.book._id,
+						errors: errors.array(),
+						bookinstance: bookinstance
+					})
+				})
+			return
+		} else {
+			bookinstance.save((err) => {
+				if (err) { return next(err) }
+				res.redirect(bookinstance.url);
+			})
+		}
+	}
+]
+
+
 
 // Display BookInstance delete form on GET
 
 exports.bookinstance_delete_get = (req, res) => {
-	res.send('NOT IMPLEMENTED: BookInstance delete GET');
-};
+	res.send('NOT IMPLEMENTED: BookInstance delete GET')
+}
 
 // Handle BookInstance delete on POST
 
 exports.bookinstance_delete_post = (req, res) => {
-	res.send('NOT IMPLEMENTED: BookInstance delete POST');
-};
+	res.send('NOT IMPLEMENTED: BookInstance delete POST')
+}
 
 // Display BookInstance update form on GET
 
 exports.bookinstance_update_get = (req, res) => {
-	res.send('NOT IMPLEMENTED: BookInstance update GET');
-};
+	res.send('NOT IMPLEMENTED: BookInstance update GET')
+}
 
 // Handle bookinstance update on POST
 
 exports.bookinstance_update_post = (req, res) => {
-	res.send('NOT IMPLEMENTED: BookInstance update POST');
-};
+	res.send('NOT IMPLEMENTED: BookInstance update POST')
+}
