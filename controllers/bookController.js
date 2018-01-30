1cdc2b9722a8af730a88f6e6a45fb7b11712e29f
Ryutaro Kawakami <ne200152g@gmail.com>
2018-01-30 20:28:10 +0900

    本情報更新機能の途中

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -34,7 +34,6 @@ exports.index = (req, res) => {
 // Display list of all books
 
 exports.book_list = (req, res, next) => {
-
 	Book.find({}, 'title author')
 		.populate('author')
 		.exec((err, list_books) => {
@@ -61,7 +60,7 @@ exports.book_detail = (req, res, next) => {
 		}
 	}, (err, results) => {
 		if (err) { return next(err) }
-		
+
 		if (results.book == null) {
 			const err = new Error('Book not found')
 
@@ -70,7 +69,7 @@ exports.book_detail = (req, res, next) => {
 			return next(err)
 		}
 
-		res.render('book_detail', { title: 'Title', book: results.book, book_instances: results.book_instance})
+		res.render('book_detail', { title: 'Title', book: results.book, book_instances: results.book_instance })
 	})
 }
 
@@ -113,7 +112,7 @@ exports.book_create_post = [
 	body('title', 'Title must not be empty.').isLength({ min: 1 }).trim(),
 	body('author', 'Author must not be empty.').isLength({ min: 1 }).trim(),
 	body('summary', 'Summary must not be empty.').isLength({ min: 1 }).trim(),
-	body('isbn', 'ISBN must not be empty').isLength({min: 1}).trim(),
+	body('isbn', 'ISBN must not be empty').isLength({ min: 1 }).trim(),
 
 	// Sanitize fields(using wildcard).
 
@@ -140,14 +139,14 @@ exports.book_create_post = [
 				}
 			}, (err, results) => {
 				if (err) { return next(err) }
-				
+
 				for (let i = 0; i < results.genres.length; i++) {
 					if (book.genre.indexOf(results.genres[i]._id) > -1) {
 						results.genres[i].checked = 'true'
 					}
 				}
-				res.render('book_form', { title: 'Create Book', authors: results.authors, genres: results.genres, book: book, errors: errors.array()})
-				})
+				res.render('book_form', { title: 'Create Book', authors: results.authors, genres: results.genres, book: book, errors: errors.array() })
+			})
 			return
 		} else {
 			book.save((err) => {
@@ -172,12 +171,104 @@ exports.book_delete_post = (req, res) => {
 
 // Display book update form on GET
 
-exports.book_update_get = (req, res) => {
-	res.send('NOT IMPLEMENTED: Book update GET')
+exports.book_update_get = (req, res, next) => {
+	async.parallel({
+		book: (callback) => {
+			Book.findById(req.params.id).populate('author').populate('genre').exec(callback)
+		},
+		authors: (callback) => {
+			Author.find(callback)
+		},
+		genres: (callback) => {
+			Genre.find(callback)
+		},
+	}, (err, results) => {
+		if (err) { return next(err) }
+
+		if (results.book == null) {
+			const err = new Error('Book not found')
+			err.status = 404
+			return next(err)
+		}
+
+		for (let all_g_iter = 0; all_g_iter < results.genres.length; all_g_iter++) {
+			for (let book_g_iter = 0; book_g_iter < results.book.genre.length; book_g_iter++) {
+				if (results.genres[all_g_iter]._id.toString() == results.book.genre[book_g_iter].id.toString()) {
+					results.genres[all_g_iter].checked = 'true'
+				}
+			}
+		}
+
+		res.render('book_form', { title: 'Update Book', authors: results.authors, genres: results.genres, book: results.book })
+	})
 }
 
 // Handle book update on POST
 
-exports.book_update_post = (req, res) => {
-	res.send('NOT IMPLEMENTED: Book update POST')
-}
+exports.book_update_post = (req, res) => [
+	(req, res, next) => {
+		if (!(req.body.genre instanceof Array)) {
+			if (typeof req.body.genre === 'undefined') {
+				req.body.genre = []
+			} else {
+				req.body.genre = new Array(req.body.genre)
+			}
+		}
+		
+		next()
+	},
+
+	//	Validate
+
+	body('title', 'Title must not be empty').isLength({ min: 1 }).trim(),
+	body('author', 'Author must not be empty.').isLength({ min: 1 }).trim(),
+	body('author', 'Summary must not be empty.').isLength({ min: 1 }).trim(),
+	body('author', 'ISBN must not be empty.').isLength({ min: 1 }).trim(),
+
+	// Sanitize
+
+	sanitizeBody('title').trim().escape(),
+	sanitizeBody('author').trim().escape(),
+	sanitizeBody('summary').trim().escape(),
+	sanitizeBody('isbn').trim().escape(),
+	sanitizeBody('genre.*').trim().escape(),
+
+	(req, res, next) => {
+		const errors = validationResult(req)
+
+		const book = new Book({
+			title: req.body.title,
+			author: req.body.author,
+			summary: req.body.summary,
+			isbn: req.body.isbn,
+			genre: (typeof req.body.genre === 'undefined') ? [] : req.body.genre,
+			_id: req.params.id
+		})
+
+		if (!errors.isEmpty()) {
+			async.parallel({
+				authors: (callback) => {
+					Author.find(callback)
+				},
+				genres: (callback) => {
+					Genre.find(callback)
+				}
+			}, (err, results) => {
+				if (err) { return next() }
+
+				for (let i = 0; i < results.genres.length; i++) {
+					if (book.genres.indexOf(results.genres[i]._id) > -1) {
+						results.genres[i].checked = 'true'
+					}
+				}
+
+				res.render('book_form', { title: 'Update Book', authors: results.authors, genres: results.genres, book: book, errors: errors.array() })
+			})
+			return
+		} else {
+			Book.findByIdAndUpdate(req.params.id, book, {}, (err, thebook) => {
+				res.redirect(thebook.url)
+			})
+		}
+	}
+]
